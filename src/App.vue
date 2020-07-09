69b345779907713a3561b676d7c06e395f3f3eb0
Dpopkova <daria.ol.p@gmail.com>
2020-07-09 17:43:12 +0300

    new commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <input v-model="text"/>
+    <input v-model="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      text: ""
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: black;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+}
+</style>
